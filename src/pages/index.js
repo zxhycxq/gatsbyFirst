756c079005f592f9aadeac5690cfcc618d231539
zxhycxq <lcyxlqn@163.com>
2019-08-25 22:22:52 +0800

    txt

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>你好呀</h1>
+    <p>欢迎来到 Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
