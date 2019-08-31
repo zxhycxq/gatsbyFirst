76890e660a675d4b6cd4d1e87f9d6d9ef3b2b23e
zxhycxq <lcyxlqn@163.com>
2019-08-31 13:01:27 +0800

    描述: bdtj

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
 			// 百度统计站点ID
       siteid: "7a9a59bc96ed24f18da5f20aa2a183df",
       // 配置统计脚本插入位置，默认值为 false, 表示插入到 body 中, 为 true 时插入脚本到 head 中
-      head: true,
+      head: false,
     },
   },
     {
